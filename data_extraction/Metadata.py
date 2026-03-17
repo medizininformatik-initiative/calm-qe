a53ae65dc07d7400c584dd85c3d11cf46ee48506
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2026-03-17 13:43:24 +0100

    feat: rename keys in metadata

--- data_extraction/Metadata.py
+++ data_extraction/Metadata.py
@@ -5,25 +5,25 @@ from datetime import datetime
 
 
 def gather_metadata(source, count):
+    metadata_file_path = 'fhir_results/metadata.json'
 
-    if os.path.exists('fhir_results/metadata.json'):
-        with open('fhir_results/metadata.json', 'r') as metadata_file:
+    if os.path.exists(metadata_file_path):
+        with open(metadata_file_path, 'r') as metadata_file:
             metadata = json.load(metadata_file)
     else:
         metadata = {
             "execution_date": datetime.now().strftime("%Y-%m-%d"),
             "execution_time": datetime.now().strftime("%H:%M:%S"),
-            "total_diagnosed_patients_with_asthma_or_copd_count": 0,
-            "primary_diagnosed_patients_with_asthma_or_copd": 0,
-            "patient_counts_with_age_interval": 0,
+            "total_asthma_or_copd_diagnosed_patients": 0,
+            "main_diagnosis_asthma_or_copd_filter": 0,
+            "main_diagnosis_encounter_count": 0,  # Not same as main_diagnosis_asthma_or_copd_filter (might be higher). Sums the total number of main diagnoses a patient has received across all encounters, regardless of the specific diagnosis codes or times.
+            "patient_count_by_age_interval": 0,  # todo: modify as dict
             "intensive_care_unit_patient_count": 0,
-            "patient_count_with_secondary_conditions": 0,
-            "patient_count_with_observations": 0,
-            "patient_count_with_medicationRequests": 0,
-            "patient_count_with_medicationAdministrations": 0,
-            "patient_count_with_medicationStatements": 0,
-            "main_diagnosis_count": 0,  # Not same as asthma_and_copd_patients_with_chief_complaint. (might higher)
-            # When same patient main diagnosed at different times at different encounter (even for different code) etc.
+            "secondary_conditions_patient_count": 0,
+            "observations_patient_count": 0,
+            "medicationRequests_patient_count": 0,
+            "medicationAdministration_patient_count": 0,
+            "medicationStatement_patient_count": 0,
             "main_diagnosis_counts": defaultdict(int),
             "secondary_conditions_counts": defaultdict(int),
             "observations_counts": defaultdict(int),
@@ -37,10 +37,14 @@ def gather_metadata(source, count):
 
     if source in metadata:
         metadata[source] = count
+    elif '_counts' in source.lower():
+        metadata[source] = defaultdict(int, {count: 0})
+        print(f"Source '{source}' was not defined; but it has been created in Metadata.json file.")
     else:
-        print(f"Source {source}, not defined with Metadata.json file.")
+        metadata[source] = count
+        print(f"Source '{source}' was not defined; but it has been created in Metadata.json file.")
 
-    with open('fhir_results/metadata.json', 'w') as metadata_file:
+    with open(metadata_file_path, 'w') as metadata_file:
         json.dump(metadata, metadata_file, indent=4)
 
     print("Metadata has been saved")
