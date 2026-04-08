90ae3ed38e166710ff17b1fa256d9c110c80f6d6
NAS-Ragde <nayeli.anaid.silva@gmail.com>
2026-04-08 18:13:37 +0200

    feat: renamed file for additional filters and adjust pipeline.

--- data_extraction/CohortPatientsAdditionalFilters.py
+++ data_extraction/CohortPatientsAdditionalFilters.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 from Constants import USER_NAME, USER_PASSWORD
 from FhirHelpersUtils import connect_to_server
-from FhirHelpersCohortExtraction import patients_with_asthma_copd, filter_icu_patients_admission, \
+from FhirHelpersCohortExtraction import extract_additional_attributes_from_encounters, filter_icu_patients_admission, \
     calculate_los_inpatients, extract_last_three_encounter, get_demographics_patients, filter_patients_by_age_interval
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
@@ -11,10 +11,10 @@ logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(message)s')
 """"
 This script is for creating the Cohort Study Data. Study protocol requires the analysis of the patients from HauptDiagnosis with Asthma & COPD.
 Here, script first finds the patients with Asthma or Copd diagnosed. And later it filters only for HauptDiagnosis from their Encounter references.
-Results are saved in "patient_results.txt"
+Results are saved in "additional_results.txt"
 """
 
-DIR_RESULTS = Path('patient_results')
+DIR_RESULTS = Path('additional_results')
 DIR_RESULTS.mkdir(exist_ok=True)
 
 
@@ -22,8 +22,9 @@ def main():
     # Connect to FHIR Server
     smart = connect_to_server(user=USER_NAME, pw=USER_PASSWORD)
 
-    # Get the patients with ANY TYPE OF DIAGNOSES related with Asthma or COPD.
-    diagnoses_filepath, encounters_filepath = patients_with_asthma_copd(smart, DIR_RESULTS)
+    # Extract attributes from encounters
+    diagnoses_filepath = Path(DIR_RESULTS, 'patients_diagnosed_asthma_copd.json')
+    encounters_filepath = extract_additional_attributes_from_encounters(smart, diagnoses_filepath)
 
     # Filter by patients' age  min_age: minimal age in years, max_age:  maximal age in years Example: [0-2], [3-5], etc.
     age_interval = {
